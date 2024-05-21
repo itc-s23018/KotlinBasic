c6448fb398d78f97868b3d1263d7d3c0616d76c4
itc-s23018 <s23018@std.it-college.ac.jp>
2024-05-21 11:38:33 +0900

    26章終了

--- src/main/kotlin/Chap26.kt
+++ src/main/kotlin/Chap26.kt
@@ -4,41 +4,74 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 fun main(args: Array<String>) {
-    val book = Book(
-        "優しいKotlin入門","野崎　英一", LocalDate.of(2018, 5, 10),
-        RentalInfo("タケシ", LocalDateTime.now(),
-            LocalDateTime.of(2024,5,21,12,0,0))
+
+    val book = Book(100, "やさしい Kotlin 入門", "野崎英一",
+        LocalDate.of(2018, 5, 10))
+
+    val rental = Rental(
+        book.id,
+        "タケシ",
+        LocalDateTime.now(),
+        LocalDateTime.of(2024,5,28,12,30)
     )
+
+    val bookWithRental = BookWithRental(
+        book, rental
+    )
+
+    val bookInfo = BookInfo(
+        bookWithRental.book.id,
+        bookWithRental.book.title,
+        bookWithRental.book.author,
+        bookWithRental.isRental
+
+    )
+
+    bookInfo.printInfo()
+    
 }
 
-class Book(
-    val title : String,
-    val author : String,
-    val releaseDate: LocalDate,
-    val rentalInfo: RentalInfo
+class BookInfo(
+    val id: Long,           //Bookクラスのidと同じ
+    val title: String,      //Bookクラスのtitleと同じ
+    val author: String,     //Bookクラスのauthor と同じ
+    val isRental: Boolean,  // BookWithRentalの　isRentalと同じ
 ){
+    constructor(bookWithRental: BookWithRental) : this(
+        bookWithRental.book.id,
+        bookWithRental.book.title,
+        bookWithRental.book.author,
+        bookWithRental.isRental
+    )
+
+
     fun printInfo(){
         println("""
-            ----------------------------------
-            書籍情報
-            ----------------------------------
-            $title
-            $releaseDate 初版
-            著者 $author
-            -----------------------------------
-            貸出情報
-            -----------------------------------
-            借りてるユーザー: ${rentalInfo.userName}
-            借りた日:${rentalInfo.rentalDate}
-            返却期限 ${rentalInfo.returnDeadline}
+            [ID:${id}] $title (${author}) 
+            ${if (isRental) "貸出中です" else "貸出可能です"}
         """.trimIndent())
     }
 }
 
-class RentalInfo (
-    val userName : String,
-    val rentalDate : LocalDateTime,
-    val returnDeadline :LocalDateTime
-){
+class Book(
+    val id: Long,
+    val title : String,
+    val author : String,
+    val releaseDate: LocalDate,
+)
 
+class Rental(
+    val bookId: Long,
+    val userName: String,
+    val rantalDateTime: LocalDateTime,
+    val rentalDeadline: LocalDateTime,
+)
+
+class BookWithRental(
+    val book: Book,
+    val rental: Rental?
+){
+    val isRental: Boolean
+    get() = rental != null
 }
+
